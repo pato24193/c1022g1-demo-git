c8cb0f7448facf0c9c0672a030b6c2e95824583f
Pato <pato24193@gmail.com>
2022-11-03 09:50:49 +0700

    add many files

---
+++ CharacterConst.java
@@ -0,0 +1,10 @@
+package com.codegym.constant;
+
+public class CharacterConst {
+    public static final String DASH = "-";
+    public static final String UNDERSCORE = "_";
+    public static final String POINT = ".";
+    public static final String BLANK = "";
+    public static final String SLASH = "/";
+    public static final String SPACE = " ";
+}
